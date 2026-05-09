4151483be8231653eb19ce4745cea0b6bd66ad9a
Dominic Boban <dominic457@outlook.com>
2026-05-09 23:15:09 +0100

    Clean up Radar dashboard performance

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,13 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      "/api/radar-dashboard": {
+        target: "https://www.dominic-boban.com",
+        changeOrigin: true,
+        secure: true,
+      },
+    },
+  },
 });
